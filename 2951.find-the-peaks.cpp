2279068a8a2602f0655e2e7122e94eb30cea539b
Reina <383821630@qq.com>
2024-05-28 20:34:21 +0800

    📌 2024.5.28

---
+++ 2951.find-the-peaks.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=2951 lang=cpp
+ * @lcpr version=30202
+ *
+ * [2951] 找出峰值
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        int n = mountain.size();
+        vector<int> ans;
+        for (int i = 1; i < n - 1; ++i)
+            if (mountain[i] > mountain[i - 1] and mountain[i] > mountain[i + 1])
+                ans.push_back(i);
+        return ans;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,4,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,4,3,8,5]\n
+// @lcpr case=end
+
+ */
+
