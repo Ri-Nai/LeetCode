4b0acf3010885f893580264bab6747fff054ea29
Ri-Nai <383821630@qq.com>
2024-06-06 21:29:41 +0800

    ✨ 2024.6.6

---
+++ 2938.separate-black-and-white-balls.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=2938 lang=cpp
+ * @lcpr version=30203
+ *
+ * [2938] 区分黑球与白球
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        int now = 0;
+        long long ans = 0;
+        for (auto c : s)
+        {
+            if (c == '0')
+                ans += now;
+            else ++now;
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "101"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "100"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "0111"\n
+// @lcpr case=end
+
+ */
+
